3d02c6a39e612f6e09dd5891db66e9ca70075cdd
JesseyEagan1 <jessey.eagan@gmail.com>
2016-02-08 20:25:29 -0700

    changed port in index.js

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ app.post('/submit', function(req, res) {
 });
 
 // Creating Server and Listening for Connections \\
-var port = 3000;
-//var port    =   process.env.PORT || 8080;
+//var port = 3000;
+var port    =   process.env.PORT || 8080;
 app.listen(port, function(){
     console.log('Server running on port ' + port);
 
